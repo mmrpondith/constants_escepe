56653848f654c24d8643154be943c68b04deb2ab
“rahulislamitibd” <itibdd@gmail.com>
2021-02-12 11:16:25 +0600

    constants_escepe

---
+++ constants_escepe.go
@@ -0,0 +1,26 @@
+package main 
+import "fmt"
+func main(){
+/*
+escepe and constants function
+*/
+//var a= 20
+const a=20
+var b=524
+var c=32.555
+var d="tomar name ki \ntianr "
+var f="tomar name ki \t tianr "
+var e="tomar name ki \a tianr "
+
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println (c)
+fmt.Println (d)
+fmt.Println (e)
+fmt.Println (f)
+fmt.Printf("a is one kinds of %T\n",a)
+fmt.Printf("b is one kinds of %T\n",b)
+fmt.Printf("c is one kinds of %T\n",c)
+
+
+}
